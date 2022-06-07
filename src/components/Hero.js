dfc851cb19d6d3548a4f6d49a2adccab73f4209b
Moshood Sanusi <snsmoshood@gmail.com>
2022-06-07 11:17:30 +0000

    hero image edit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,12 +9,10 @@ const Hero = () => {
             We are committed to helping our customers provide <span className='hero-h-text'>healthy,</span>
             <span className='hero-h-text'> nutritious</span> food for their <span className='hero-h-text'> family.</span>  
           </p>
-          <p className='hero-s-text'>
-            At Adiye we are more than chicken were protein leaders. 
-          </p>
+          <p className='hero-s-text'>food innovators who thrive at the intersection of opportunity by upholding the highest standards in poultry.</p>
           <button className='hero-btn'><a href="" >Contact</a></button>
           <button className='hero-btn'><a href="" >Order now</a></button>
-           {/* <div><img src="/images/hero-img.svg" alt="" /></div> */}
+          <img src="/images/hero-img.svg" alt="" className='hero-img' />
         </div>
         {/* <div><img src="" alt="" /></div> */}
       </div>
