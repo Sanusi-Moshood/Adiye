a929e287f496deb336131df14982e73fc18d5613
Moshood Sanusi <snsmoshood@gmail.com>
2022-06-16 15:09:37 +0000

    finished team section

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,6 +5,7 @@ const Team = () => {
     <div>
       <div className="container">
           <div className="team-container">
+              <div className="team-container-2">
               <div className="team">
                   <div className="team-content">
                       <div className="team-header">
@@ -16,32 +17,35 @@ const Team = () => {
                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin 
                           consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet 
                           etLorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur 
-                          justo quis euismod vehicula. Quisque diam dui, imperdiet etLorem ipsum dolor 
-                          sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula.
-                          Quisque diam dui, imperdiet et
                           </p>
                       </div>
+                      <div>
+                        <a href="" className='btn-transparent btn-team'>Learn more</a> 
+                      </div>
+
                   </div>
 
               </div>
               <div className="invest">
               <div className="team-content">
                       <div className="team-header">
-                          <h1>Join our team</h1>
-                          <p>Over 1,000 job opportunities and counting</p>
+                          <h1>Invest with us</h1>
+                          <p>Over 1,000 investors from different countries</p>
                       </div>
                       <div className="team-text">
                           <p>
                           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin 
                           consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet 
                           etLorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur 
-                          justo quis euismod vehicula. Quisque diam dui, imperdiet etLorem ipsum dolor 
-                          sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula.
-                          Quisque diam dui, imperdiet et
                           </p>
+                      </div>
+                      <div>
+                        <a href="" className='btn-transparent btn-team'>Learn more</a> 
                       </div>
                   </div>
               </div>
+              </div>
+
           </div>
       </div>
     </div>
