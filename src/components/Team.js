99cf191b1a0df09f7a2f41706ce0fe092f1eb9eb
Moshood Sanusi <snsmoshood@gmail.com>
2022-06-16 04:07:37 +0000

    team edit

---
+++ src/components/Team.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+      <div className="container">
+          <div className="team-container">
+              <div className="team">
+                  <div className="team-content">
+                      <div className="team-header">
+                          <h1>Join our team</h1>
+                          <p>Over 1,000 job opportunities and counting</p>
+                      </div>
+                      <div className="team-text">
+                          <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin 
+                          consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet 
+                          etLorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur 
+                          justo quis euismod vehicula. Quisque diam dui, imperdiet etLorem ipsum dolor 
+                          sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula.
+                          Quisque diam dui, imperdiet et
+                          </p>
+                      </div>
+                  </div>
+
+              </div>
+              <div className="invest">
+              <div className="team-content">
+                      <div className="team-header">
+                          <h1>Join our team</h1>
+                          <p>Over 1,000 job opportunities and counting</p>
+                      </div>
+                      <div className="team-text">
+                          <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin 
+                          consectetur justo quis euismod vehicula. Quisque diam dui, imperdiet 
+                          etLorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur 
+                          justo quis euismod vehicula. Quisque diam dui, imperdiet etLorem ipsum dolor 
+                          sit amet, consectetur adipiscing elit. Proin consectetur justo quis euismod vehicula.
+                          Quisque diam dui, imperdiet et
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Team
