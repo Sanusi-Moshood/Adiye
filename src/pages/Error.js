7d5894f34f3cbf4230c0b329e0e4437f22359258
Moshood Sanusi <snsmoshood@gmail.com>
2022-06-29 20:50:14 +0000

    few chages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
     <h1 className='error'>
-      Error 404 
+      Error <br /> 
+        Page cant be found <br />
+    <Link to='/' href="">Back to home</Link>
     </h1>
   )
 }
